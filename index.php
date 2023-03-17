8f265dac79c2dc77999fd3778818fa39f12d1bf4
Slava <terminatorslava38@gmail.com>
2023-03-17 15:00:40 +0300

    ff

--- index.php
+++ index.php
@@ -17,3 +17,23 @@
 
 </body>
 </html>
+[
+            "SELECT
+                 catalog.id,
+                 ROUND(cost * items.count, 2) as cost,
+                 IFNULL(items.count, 0) as quantity,
+                 IFNULL(catalog.provider, '') as id_vendor,
+                 catalog.category,
+                 set_id,
+                 set_name,
+                 ROUND((items.new_price - (items.new_price * sets.paid_bonus_percent)), 2) as new_price,
+                 ROUND(items.old_price, 2) as old_price,
+                 ROUND((items.total_sum - (items.total_sum * sets.paid_bonus_percent)), 2) as total_sum,
+                 ROUND(catalog.price, 2) as price,
+                 sets.date_create,
+                 sets.payment_method
+             FROM catalog_sets_items items",
+            ' LEFT JOIN catalog ON catalog.id = items.catalog_id ',
+            ' LEFT JOIN catalog_sets sets ON sets.id = items.set_id ',
+            " WHERE items.set_id IN (${catalogSetsPlainIds})",
+        ]
